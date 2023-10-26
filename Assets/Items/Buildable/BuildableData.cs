00e7372992530379d2ebe6864d78e25b7e675e21
Guill <guillaume.piette@student.vinci.be>
2023-10-26 18:01:11 +0200

    UI adaptative

---
+++ Assets/Items/Buildable/BuildableData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildableData", menuName = "My Game/New Item/Buildable")]
+public class Buildable : ItemData
+{
+    
+}
